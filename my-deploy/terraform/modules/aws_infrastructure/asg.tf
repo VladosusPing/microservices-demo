cbccce7d4804001a53c21c4a971e13c950370e1e
Nadya <nadyassha@gmail.com>
2025-02-10 18:37:17 +0400

    fixed errors, applied successfully

--- my-deploy/terraform/modules/aws_infrastructure/asg.tf
+++ my-deploy/terraform/modules/aws_infrastructure/asg.tf
@@ -1,8 +1,8 @@
 resource "aws_autoscaling_group" "eks_asg" {
   vpc_zone_identifier = [
-    aws_subnet.prod-public-subnet-us-east-1a.id, 
-    aws_subnet.prod-public-subnet-us-east-1b.vpc_id
-    ]
+    aws_subnet.prod-public-subnet-us-east-1a.id,
+    aws_subnet.prod-public-subnet-us-east-1b.id
+  ]
 
   desired_capacity = 1
   min_size         = 1
