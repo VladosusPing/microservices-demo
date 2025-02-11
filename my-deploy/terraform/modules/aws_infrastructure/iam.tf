14e779c400bc73b98a24b3056ae2c31258294c7d
VladosusPing <vlad.ap15@gmail.com>
2025-02-11 19:36:47 +0400

    fixed repo name

--- my-deploy/terraform/modules/aws_infrastructure/iam.tf
+++ my-deploy/terraform/modules/aws_infrastructure/iam.tf
@@ -44,7 +44,7 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
           },
           "StringLike" : {
-            "token.actions.githubusercontent.com:sub" : "repo:VladosusPing/rsschool-devops-course-tasks:*"
+            "token.actions.githubusercontent.com:sub" : "repo:VladosusPing/microservices-demo:*"
           }
         }
       }
